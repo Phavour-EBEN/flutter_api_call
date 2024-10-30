26740e137c136f654bbb7234a61ee8533e06561f
Manasseh Kabutey <107959408+Qharny@users.noreply.github.com>
2024-10-30 16:36:15 +0000

    Update homeScreen.dart

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -3,8 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
-
-  const HomeScreen({ super.key });
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -13,45 +12,51 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   List<dynamic> users = [];
 
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-         appBar: AppBar(
-           title: const Text('API CALLS'),
-         ),// appBar
-         body: ListView.builder(
-          itemCount: users.length,
-          itemBuilder: (context, index){
-            final user = users[index];
-            final name = user['name']['first'];
-            final email = user['email'];
-            final imgUrl = user['picture']['thumbnail'];
-            return ListTile(
-              leading: ClipRRect(
-                borderRadius: BorderRadius.circular(100),
-                child: Image.network(imgUrl),
-              ),
-              title: Text(name),
-              subtitle: Text(email),
-            );//listTile
-          },
-         ),
-         floatingActionButton: FloatingActionButton(
-          onPressed: fetchUsers,
-         )//floatingActionButton
-       );//Scaffold
+  @override
+  void initState() {
+    super.initState();
+    fetchUsers();
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('API CALLS'),
+      ),
+      body: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context, index) {
+          final user = users[index];
+          final name = user['name']['first'];
+          final email = user['email'];
+          final imgUrl = user['picture']['thumbnail'];
+          return ListTile(
+            leading: ClipRRect(
+              borderRadius: BorderRadius.circular(100),
+              child: Image.network(imgUrl),
+            ),
+            title: Text(name),
+            subtitle: Text(email),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: fetchUsers,
+      ),
+    );
+  }
+
   Future<void> fetchUsers() async {
     print('fetch users called');
-    const url = 'https://randomuser.me/api/?results=10';
+    final url = 'https://randomuser.me/api/?results=10';
     final uri = Uri.parse(url);
     final response = await http.get(uri);
     final body = response.body;
-    final json = jsonDecode(body); // Use dart:convert's jsonDecode
+    final json = jsonDecode(body);
     setState(() {
       users = json['results'];
     });
     print('Fetch Users completed');
   }
 }
-
